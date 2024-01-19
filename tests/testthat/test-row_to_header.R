5ae1f1153c2269511419806a44db9063de2e958b
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-01-19 10:39:45 +0100

    Grouped cases for first and second columns in typical cases test in row_to_header function, contains both expectations. Introduced test for proper error when out of bounds row is passed to function

--- tests/testthat/test-row_to_header.R
+++ tests/testthat/test-row_to_header.R
@@ -1,13 +1,9 @@
-test_that("Works in most basic case", {
+test_that("Works in typical cases", {
   df <- data.frame(c("Animal", "Fish"), c("Item", "Table"))
-  expected_res <- data.frame(Animal="Fish", Item="Table")
-  expect_equal(row_to_header(df), expected_res)
-})
-
-test_that("Also works with second row", {
-  df <- data.frame(c("Animal", "Fish"), c("Item", "Table"))
-  expected_res <- data.frame(Fish="Animal", Table="Item")
-  expect_equal(row_to_header(df, 2), expected_res)
+  expected_res_1 <- data.frame(Animal="Fish", Item="Table")
+  expected_res_2 <- data.frame(Fish="Animal", Table="Item")
+  expect_equal(row_to_header(df), expected_res_1)
+  expect_equal(row_to_header(df, 2), expected_res_2)
 })
 
 test_that("Keeps rownames when update_rows is FALSE", {
@@ -24,3 +20,9 @@ test_that("Updates rownames when update_rows is TRUE (default)", {
   expected_res <- data.frame(Animal="Fish", Item="Table")
   expect_equal(row_to_header(df), expected_res)
 })
+
+test_that("Out of bounds row returns error", {
+  df <- data.frame(c("Animal", "Fish"), c("Item", "Table"))
+  expect_error(row_to_header(df, 3), "row is out of bounds")
+  expect_error(row_to_header(df, 0), "row is out of bounds")
+  })
