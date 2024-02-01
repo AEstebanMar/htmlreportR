89be5c1268075d2bf1170fe39d9938675c144406
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-02-01 14:45:03 +0100

    Updated argument names

--- tests/testthat/test-gg_heatmap.R
+++ tests/testthat/test-gg_heatmap.R
@@ -2,6 +2,6 @@ test_that("no error when specified columns exist", {
   test <- data.frame(sample=c("P1", "C1", "P2", "C2"),
                      gene=c("MMUT2", "ASS2", "GALE", "MMUT2"),
                      logpval=1:4)
-  p <- gg_heatmap(data=test, x_axis="sample", y_axis="gene", fill="logpval")
+  p <- gg_heatmap(input_table=test, x_axis="sample", y_axis="gene", fill="logpval")
   expect_no_error(print(p))
 })
