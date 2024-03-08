e6033415d6963f6e65d445e97e3b54e8232ce3de
JoseCorCab <josecordoba@uma.es>
2024-03-08 13:08:57 +0100

    data management has been adapted from pyReportHtml

--- R/general_functions.R
+++ R/general_functions.R
@@ -1,27 +1,7 @@
 ## Estas funciones deben eliminarse cuando cambiemos la estructutra de tablas de los report
 
 
-#' Select first row from data frame and make it its header
-#' 
-#' `parse_column_names` takes an input data frame and renames set columns in
-#' order to simplify manipulation by other DRAGGER functions.
-#' @param df A data frame of N rows.
-#' @param row An integer. Index of row to turn into header (default 1).
-#' @param update_rows A boolean.
-#'   * `TRUE` (the default): data frame row names will be updated (first column
-#'                           will be 1, second will be 2). If input df had no
-#'							 row names, they are coerced to numerical values.
-#' 							 This option ensures they will reflect their new
-#' 							 positions.
-#'   * `FALSE`:              data frame row names will be left untouched.
-#'                           useful if they had previous values.
-#' @returns A data frame of N-1 rows, where the selected row is now the header.
-#' @examples
-#' df <- data.frame(c("Animal", "Fish"), c("Item", "Table"))
-#' print(df)
-#' header_df <- row_to_header(df, 1)
-#' print(header_df)
-#' @export
+
 build_header <- function(table_data){
 	colnames(table_data) <- table_data[1,]
 	table_data <- table_data[-1,]
@@ -32,7 +12,14 @@ as_numeric_if <- function(table_data){
 #cambiar nombre cancer
 	numeric_columns <- grepl("^\\d*\\.?\\d+$", table_data[1,])
 
-	table_data[,numeric_columns] <- lapply(table_data[,numeric_columns], as.numeric)
+	table_data[,numeric_columns] <- lapply(table_data[,numeric_columns], 
+										   function(col) {
+										   	if (!is.factor(col)){
+										   		as.numeric(col) 
+										   	} else {
+										   		col
+										   	}})
+										   	
 	return(table_data)
 } 
 	
--- R/htmlreport.R
+++ R/htmlreport.R
@@ -1,20 +1,23 @@
 setClass("htmlReport", 
   slots = c(
+    hash_vars = "list",
     all_report = "character", 
     title = "character",
-    hash_vars = "list",
     tmp_dir = "character"
   ),
    prototype = list(
+    hash_vars = list(),
     all_report = "", 
     title = "",
-    hash_vars = list(),
     tmp_dir = ""
   )
 )
 
 
-`%+%` <- function(a, b) paste0(c(a, b), collapse="")
+setGeneric("%+%", function(plotter, value) standardGeneric("%+%"))
+setMethod("%+%", "htmlReport", function(plotter, value) {
+	paste(c(plotter@all_report,	value), collapse = "")
+})
 
 setGeneric("build", function(plotter, template) standardGeneric("build"))
 setMethod("build", "htmlReport", function(plotter, template) {
@@ -22,10 +25,10 @@ setMethod("build", "htmlReport", function(plotter, template) {
 	knitr::opts_chunk$set(echo = FALSE, results="asis", message=FALSE, error = FALSE, warning = FALSE)
 	rendered_template <- knitr::knit(text = templ, quiet = TRUE)
 
-	paste_report(plotter) <- "<HTML>\n"
+	plotter@all_report <- plotter %+% "<HTML>\n"
 	plotter <- make_head(plotter)
 	plotter <- build_body(plotter, rendered_template)
-  paste_report(plotter) <- "\n</HTML>"
+  plotter@all_report <- plotter %+% "\n</HTML>"
   return(plotter)
 })
 
@@ -41,14 +44,14 @@ setMethod("paste_report<-", "htmlReport", function(plotter, value) {
 setGeneric("make_head", function(plotter) standardGeneric("make_head"))
 setMethod("make_head", "htmlReport", function(plotter) {
 	title <- plotter@title
-	paste_report(plotter) <- c("\t<title>", title, "</title>\n<head>\n")
-	paste_report(plotter) <- "</head>\n"
+	plotter@all_report <- plotter %+% c("\t<title>", title, "</title>\n<head>\n")
+	plotter@all_report <- plotter %+% "</head>\n"
   return(plotter)
 })
 
 setGeneric("build_body", function(plotter, body_text) standardGeneric("build_body"))
 setMethod("build_body", "htmlReport", function(plotter, body_text) {
-	paste_report(plotter) <- body_text
+	plotter@all_report <- plotter %+% body_text
     return(plotter)
 })
 
@@ -59,44 +62,171 @@ setMethod("write_report", "htmlReport", function(plotter, output_path) {
 })
 
 
-setGeneric("static_ggplot_main", function(plotter, id_plot, header = FALSE, plotting_function = NULL, ...) standardGeneric("static_ggplot_main"))
-setMethod("static_ggplot_main", "htmlReport", function(plotter, id_plot, header = FALSE, plotting_function = NULL, ...) {
-	data_frame <- plotter@hash_vars[[id_plot]]
+setGeneric("static_plot_main", function(
+plotter, 
+id, 
+header = FALSE, 
+row_names = FALSE,
+transpose = FALSE,
+smp_attr = NULL,
+var_attr = NULL,
+fields = NULL,
+func = NULL,
+plotting_function = NULL
+) standardGeneric("static_plot_main"))
+setMethod("static_plot_main", "htmlReport", function(
+plotter, 
+id, 
+header = FALSE, 
+row_names = FALSE,
+transpose = FALSE,
+smp_attr = NULL,
+var_attr = NULL,
+fields = NULL,
+func = NULL,
+plotting_function = NULL) {
 
-	if (header) data_frame <- build_header(data_frame) 
+	options <- list(id = id,
+									header = header,
+									row_names = row_names,
+									transpose = transpose,
+									smp_attr = smp_attr,
+									var_attr = var_attr,
+									fields = fields,
+									func = func)
+	data_frame <- get_data_for_plot(plotter, options)$data_frame
+	save(list = ls(),file = "/mnt/home/users/bio_267_uma/josecordoba/software/htmlreportR/tests/check_plots/test.Rdata")
 
-	ggplot_obj <- ggplot2::ggplot(data_frame)
-	if (is.null(plotting_function )) return(ggplot_obj)
+	if(is.null(plotting_function)) return(data_frame)
+	plot_obj <- plotting_function(data_frame)
+	get_plot(plotter, plot_obj)
+})
 
-	ggplot_obj <- plotting_function(ggplot_obj, ...)
+setGeneric("static_ggplot_main", function(
+plotter, 
+id, 
+header = FALSE, 
+row_names = FALSE,
+transpose = FALSE,
+smp_attr = NULL,
+var_attr = NULL,
+fields = NULL,
+func = NULL,
+plotting_function = NULL) standardGeneric("static_ggplot_main"))
+setMethod("static_ggplot_main", "htmlReport", function(
+plotter, 
+id, 
+header = FALSE, 
+row_names = FALSE,
+transpose = FALSE,
+smp_attr = NULL,
+var_attr = NULL,
+fields = NULL,
+func = NULL,
+plotting_function = NULL) {
+	ggplot_f <- function(data_frame, plotting_function_gg = plotting_function){
+				ggplot_obj <- ggplot2::ggplot(data_frame)
+				plotting_function_gg(ggplot_obj)
+	}
 
-get_plot(plotter, ggplot_obj)
-	})
+	static_plot_main(plotter =plotter,
+									id = id,
+									header = header,
+									row_names = row_names,
+									transpose = transpose,
+									smp_attr = smp_attr,
+									var_attr = var_attr,
+									fields = fields,
+									func = func, 
+									plotting_function = ggplot_f)
+})
 
 setGeneric("get_plot", function(plotter, plot_obj) standardGeneric("get_plot"))
 setMethod("get_plot", "htmlReport", function(plotter, plot_obj) {
 	#This code writes the plot to a temporal png, then it load the png in base64 encoding and then 
 	
 	file_png <- file.path(plotter@tmp_dir, "tmp_fig.png")
-  message(file_png)
-
   png(file_png, 
 		width = knitr::opts_current$get("fig.width"),
 		height = knitr::opts_current$get("fig.height"),
 		units = "in",
 		res = 200)
-
 	  plot(plot_obj)
-	
 	dev.off()
-	enc_img <- embed_img(plotter, file_png)
+	enc_img <- embed_file(file_png)
 	cat(paste0("\n<img src=", enc_img, " />"))
 	
 })
 
 
-setGeneric("embed_img", function(plotter, img_file) standardGeneric("embed_img"))
-setMethod("embed_img", "htmlReport", function(plotter, img_file) {
-    enc_img <- xfun::base64_uri(img_file)
-    return(enc_img)
+embed_file <- function(img_file) {
+    xfun::base64_uri(img_file)
+}
+
+
+setGeneric("get_data_for_plot", function(plotter, options) standardGeneric("get_data_for_plot"))
+setMethod("get_data_for_plot", "htmlReport", function(plotter, options) {
+		all_data <- get_data(plotter, options)
+		all_data <- c(all_data,
+								list(samples = colnames(all_data$data_frame),
+										variables = rownames(all_data$data_frame)))
+		return(all_data)
+})
+
+
+setGeneric("get_data", function(plotter, options) standardGeneric("get_data"))
+setMethod("get_data", "htmlReport", function(plotter, options) {
+		data_frame <- plotter@hash_vars[[options$id]]
+		#add_header_row_names
+		data_frame <- add_header_row_names(plotter, data_frame, options)
+
+		#transpose
+		if (options$transpose) data_frame <- as.data.frame(t(data_frame))
+		#extract data
+		all_data <- extract_data(plotter, data_frame, options)
+		#modification function
+		if (!is.null(options$func)) all_data$data_frame <- options$func(all_data$data_frame)
+		return(all_data)				
+})
+
+setGeneric("extract_data", function(plotter, data_frame, options) standardGeneric("extract_data"))
+setMethod("extract_data", "htmlReport", function(plotter, data_frame, options) {	
+		smp_attr <- NULL #length(NULL) ==> 0
+    var_attr <- NULL
+    
+    if (length(options$smp_attr) > 0){
+    	smp_attr <- data_frame[,options$smp_attr, drop = FALSE]
+    	data_frame <- data_frame[,-options$smp_attr, drop = FALSE]
+    } 
+    if (length(options$var_attr) > 0){
+    	var_attr <- data_frame[options$var_attr,, drop = FALSE]
+    	data_frame <- data_frame[-options$var_attr,, drop = FALSE]
+    }
+    
+    if (length(options$smp_attr) > 0 && 
+    		length(options$var_attr) > 0){
+    	smp_attr <- smp_attr[-options$var_attr,,drop = FALSE]    	
+    } 
+    if (length(options$fields > 0))
+    data_frame <- data_frame[,options$fields, drop = FALSE]
+  	numeric_columns <- grepl("^\\d*\\.?\\d+$", data_frame[1,])
+		data_frame[,numeric_columns] <- lapply(data_frame[,numeric_columns], as.numeric)
+
+		return(list(data_frame = data_frame,
+								smp_attr = smp_attr,
+								var_attr = var_attr))
+})
+
+
+setGeneric("add_header_row_names", function(plotter, data_frame, options) standardGeneric("add_header_row_names"))
+setMethod("add_header_row_names", "htmlReport", function(plotter, data_frame, options) {	
+		if (options$header) {
+			colnames(data_frame) <- data_frame[1,]
+			data_frame <- data_frame[-1,]
+		}
+		if (options$row_names) {
+			rownames(data_frame) <- data_frame[,1]
+			data_frame <- data_frame[,-1]
+		}
+		return(data_frame)	
 })
