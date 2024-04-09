a02596c6bbb0b885b955424e4170a62b483bb926
JoseCorCab <josecordoba@uma.es>
2024-04-09 10:45:34 +0200

    Null files are now allowed

--- R/main_htmlreport.R
+++ R/main_htmlreport.R
@@ -1,9 +1,12 @@
 #' @importFrom methods new
 #' @importFrom utils read.table
 main_htmlreportR <- function(options){
-	table_list <- strsplit(options$data_files, ",")[[1]]
-	data_files <- lapply(table_list, utils::read.table, header = FALSE)
-	names(data_files) <- sapply(table_list, basename)
+	data_files <- list()
+	if (!is.null(options$data_files)){
+		table_list <- strsplit(options$data_files, ",")[[1]]
+		data_files <- lapply(table_list, utils::read.table, header = FALSE)
+		names(data_files) <- sapply(table_list, basename)
+	}
 
 	if (is.null(options$output_file)){
 	  output_file <- file.path(dirname(options$template), "report.html")
