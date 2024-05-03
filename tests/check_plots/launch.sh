01c558d7aaf4466ab576d6856e842ca9347ebdad
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-05-03 16:14:12 +0200

    Added x_y.txt table to htmlreportR call

--- tests/check_plots/launch.sh
+++ tests/check_plots/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source init_htmlreportR
-../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt -t template.txt
-../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt -t template.txt -c -o report_compressed.html
+../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt -t template.txt
+../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt -t template.txt -c -o report_compressed.html
 ./launch_report_library.R 
 
