78a7861634bdf9e0cf391a2427bd16520e9ed376
JoseCorCab <josecordoba@uma.es>
2024-05-31 12:03:15 +0200

    canvasxpress version and dendrogram added

--- tests/check_plots/launch.sh
+++ tests/check_plots/launch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source init_htmlreportR
-../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt,sparse_matrix -t template.txt
- ../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt,sparse_matrix -t template.txt -c -o report_compressed.html
- ./launch_report_library.R 
+../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt,sparse_matrix -t template.txt -u
+../../inst/scripts/html_report.R -d data_toc_sample.txt,hap_sample,density_ridgeline_example.txt,x_y.txt,sparse_matrix -t template.txt  -o report_compressed.html
+./launch_report_library.R 
 
