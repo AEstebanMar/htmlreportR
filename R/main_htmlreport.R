aeeaad933a49b6f3922a688f0d8d9329193c5ee9
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-07-22 15:52:27 +0200

    Documented main_htmlreport function

--- R/main_htmlreport.R
+++ R/main_htmlreport.R
@@ -1,7 +1,27 @@
+#' Main htmlreportR scripting function
+#'
 #' @importFrom methods new
 #' @importFrom utils read.table
-#' @return
+#' @title Build report by loading files from disk and rendering template.
+#' @description Main htmlreportR script mode function. Renders a template,
+# building an htmlReport instance named plotter which contains loaded files.
+#' @param options list containing all necessary elements to build an
+#' htmlReport instance.
+#' @returns \value{None}
+#' @export
 #' @examples
+#' \dontrun{}
+#' options <- list(title = "main_htmlreportR example",
+#'				   data_files = "path/to/file1", "path/to/fileN",
+#'				   output_file = "path/to/output",
+#'				   source_folder = "path/to/package/scripts",
+#'				   compress_obj = TRUE, css_files = "path/to/custom/css",
+#'				   js_files = "path/to/custom/js",
+#'				   cdn_css = "path/to/cdn_css",
+#'				   cdn_js = "path/to/cdn_jss",
+#'				   menu = "menu")
+#' main_htmlreportR (options)
+
 main_htmlreportR <- function(options){
 	data_files <- list()
 	if (!is.null(options$data_files)){
