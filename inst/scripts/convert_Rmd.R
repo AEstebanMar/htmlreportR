8ab9d47f0ba42fe3ce231afc21f753c14413b1c7
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-10-16 11:48:57 +0200

    Script now begins with check for HTMLREPORTER_MODE ("DEVELOPMENT" or other). Updated regular expressions to include additional possible characters between paired marks. Script can now handle unsorted lists and comments inside R code. Script no longer edits input line, and no longer returns a list, instead works by growing a string

--- inst/scripts/convert_Rmd.R
+++ inst/scripts/convert_Rmd.R
@@ -1,5 +1,21 @@
 #! /usr/bin/env Rscript
 
+
+if( Sys.getenv('HTMLREPORTER_MODE') == 'DEVELOPMENT' ){
+  # Obtain this script directory
+  full.fpath <- normalizePath(unlist(strsplit(commandArgs()[grep('^--file=', 
+                  commandArgs())], '='))[2])
+  main_path_script <- dirname(full.fpath)
+  root_path <- file.path(main_path_script, '..', '..')
+  # Load custom libraries
+  devtools::load_all(file.path(root_path))
+  source_folder <- file.path(root_path, 'inst')
+}else{
+  require('htmlreportR')
+  root_path <- find.package('htmlreportR')
+  source_folder <- file.path(root_path)
+}
+
 option_list <- list(
   optparse::make_option(c("-i", "--input"), type="character", default=NULL,
     help="Path to Rmd file."),
@@ -15,37 +31,54 @@ if(is.null(opt$output)) {
 }
 
 file <- readLines(opt$input)
-
+item_environment <- "none"
+output <- character(0)
 for(line in seq(length(file))) {
-	if(grepl("```\\{r", file[line])) {
-		test <- replace_paired_mark(string = file[line],
-																pattern = "(```\\{r)(.*)(\\})",
-																replace = c("<!--begin.rcode", ""))
-	} else if (grepl("```", file[line])) {
-		file[line] <- gsub("```", "end.rcode-->", file[line])
+	current_line <- file[line]
+	if(grepl("^\\*(?!\\*)|\n\\*(?!\\*) ", current_line, perl = TRUE)) {
+		if(item_environment != "list"){
+			current_line <- paste0("<ul>\n", current_line)
+			item_environment <- "list"
+			}
+		current_line <- replace_paired_mark(string = current_line,
+																			  pattern = "(\\*)(.*)",
+																		    replace = c("<li>", "</li>"))
+		} else if(item_environment == "list"){
+			item_environment <- "none"
+			current_line <- paste0("\n</ul>\n", current_line)
+		}
+		
+	if(grepl("```\\{r", current_line)) {
+		item_environment <- "rcode"
+		current_line <- replace_paired_mark(string = current_line,
+																		pattern = "(```\\{r)(.*)(\\})",
+																		replace = c("<!--begin.rcode", ""))
 	}
-	if(grepl("#", file[line])) {
-		text <- stringr::str_match(file[line], "(#+)(.*)")
+	if(grepl("```", current_line, perl = TRUE) & item_environment == "rcode") {
+		current_line <- gsub("```", "end.rcode-->", current_line)
+		item_environment <- "none"
+	}
+	if((grepl("#", current_line) & item_environment != "rcode") | grepl("cat\\(\"#", current_line)) {
+		text <- stringr::str_match(current_line, "(#+)(.*)")
 		level <- nchar(text[2])
 		replace_1 <- paste0("<h", level, ">")
 		replace_2 <- paste0("</h", level, ">")
-		file[line] <- gsub(pattern = text[1], x = text,
-							  		   replacement = paste0(replace_1, text[1], replace_2))
+		current_line <- replace_paired_mark(string = current_line, pattern = "(#+)(.*)",
+																	  replace = c(replace_1, replace_2))
 	}
-	file[line] <- replace_paired_mark(string = file[line],
-																		pattern = paste0("(\\*\\*\\*+?)([- \\w",
-																		"]+)(\\*\\*\\*+?)"),
-																	  replace = c("<strong><em>",
-																	  					  "</em></strong>"))
-	file[line] <- replace_paired_mark(string = file[line],
-																		pattern = paste0("(\\*\\*+?)([- \\w]+)",
-																	  "(\\*\\*+?)"),
-																	  replace = c("<strong>", "</strong>"))
-	file[line] <- replace_paired_mark(string = file[line],
-																		pattern = paste0("(\\*+?)([- \\w]+)",
-																	  "(\\*\\*+?)"),
-																	  replace = c("<em>", "</em>"))
+	current_line <- replace_paired_mark(string = current_line,
+																	pattern = paste0("(\\*\\*\\*+?)([-():; \\w]+)",
+																		"(\\*\\*\\*+?)"),
+																	replace = c("<strong><em>", "</em></strong>"))
+	current_line <- replace_paired_mark(string = current_line,
+																	pattern = paste0("(\\*\\*+?)([-():; \\w]+)(\\*",
+																		"\\*+?)"),
+																	replace = c("<strong>", "</strong>"))
+	current_line <- replace_paired_mark(string = current_line,
+																	pattern = paste0("(\\*+?)([-():; \\w]+)(\\*\\*",
+																		"+?)"),
+																	replace = c("<em>", "</em>"))
+	output <- paste0(output, current_line, "\n")
 }
 
-writeLines(file, con = opt$output)
-
+cat(output, file = opt$output)
