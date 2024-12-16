8f9c71d32fe94668d892de5ac4e55663ad0ba285
Álvaro Esteban Martos <alvaroesteban@uma.es>
2024-12-16 12:15:44 +0100

    Rmd conversion script can now handle titles inside cat commands. Still has trouble in some cases, needs some more testing

--- inst/scripts/convert_Rmd.R
+++ inst/scripts/convert_Rmd.R
@@ -29,7 +29,6 @@ opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
 if(is.null(opt$output)) {
 	opt$output <- paste0(tools::file_path_sans_ext(opt$input), ".txt")
 }
-
 file <- readLines(opt$input)
 item_environment <- "none"
 output <- character(0)
@@ -47,24 +46,32 @@ for(line in seq(length(file))) {
 			item_environment <- "none"
 			current_line <- paste0("\n</ul>\n", current_line)
 		}
-		
 	if(grepl("```\\{r", current_line)) {
 		item_environment <- "rcode"
 		current_line <- replace_paired_mark(string = current_line,
 																		pattern = "(```\\{r)(.*)(\\})",
 																		replace = c("<!--begin.rcode", ""))
-	}
-	if(grepl("```", current_line, perl = TRUE) & item_environment == "rcode") {
+	}else if(grepl("```", current_line, perl = TRUE) & item_environment == "rcode") {
 		current_line <- gsub("```", "end.rcode-->", current_line)
 		item_environment <- "none"
 	}
-	if((grepl("#", current_line) & item_environment != "rcode") | grepl("cat\\(\"#", current_line)) {
+	if(grepl("#", current_line) & item_environment != "rcode") {
 		text <- stringr::str_match(current_line, "(#+)(.*)")
 		level <- nchar(text[2])
 		replace_1 <- paste0("<h", level, ">")
 		replace_2 <- paste0("</h", level, ">")
-		current_line <- replace_paired_mark(string = current_line, pattern = "(#+)(.*)",
-																	  replace = c(replace_1, replace_2))
+		current_line <- replace_paired_mark(string = current_line,
+																				pattern = "(#+)(.*)",
+																	  		replace = c(replace_1, replace_2))
+	}
+	if(grepl("cat\\(\"\\\\n*#+", current_line)) {
+		text <- stringr::str_match(current_line, "(#+)(.*)")
+		level <- nchar(text[2])
+		replace_1 <- paste0("<h", level, ">")
+		replace_2 <- paste0("</h", level, ">\")")
+		current_line <- replace_paired_mark(string = current_line,
+																			  pattern = "(\\\\n*#+)(.*)(\\\\n*\"\\))",
+																			  replace = c(replace_1, replace_2))
 	}
 	current_line <- replace_paired_mark(string = current_line,
 																	pattern = paste0("(\\*\\*\\*+?)([-():; \\w]+)",
