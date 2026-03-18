cac9101c921744405dfdd37bd9d3e90e4755d0c7
Álvaro Esteban Martos <alvaroesteban@uma.es>
2026-03-18 14:41:10 +0100

    Formatting fix

--- inst/scripts/html_report.R
+++ inst/scripts/html_report.R
@@ -19,25 +19,25 @@ if( Sys.getenv('HTMLREPORTER_MODE') == 'DEVELOPMENT' ){
 
 option_list <- list(
   optparse::make_option(c("-d", "--data_files"), type = "character", default=NULL,
-    help="Comma sepparated text files with data to use on graphs or tables within report"),
+    help = "Comma separated paths to tabular input files"),
   optparse::make_option(c("-t", "--template"), type = "character", default=NULL,
-    help="Report template"),
- optparse::make_option(c("-o", "--output_file"), type = "character", default=NULL,
-    help="HTML file path to render the template"),
- optparse::make_option(c("--title"), type = "character", default="htmlreportR",
-    help="Title of the html report"),
- optparse::make_option(c("-u", "--uncompressed_data"), type = "logical", default = TRUE, action = "store_false", 
-    help = "Indicate if figure data must be uncompressed." ),
- optparse::make_option(c("-c", "--css_files"), type = "character", default=NULL,
-    help="Path to css files that must be included. Use ',' as path separator for each file"),
+    help = "Path to template to render"),
+  optparse::make_option(c("-o", "--output_file"), type = "character", default=NULL,
+    help = "Path to file where output will be written"),
+  optparse::make_option(c("--title"), type = "character", default="htmlreportR",
+    help = "Title of the report"),
+  optparse::make_option(c("-u", "--uncompressed_data"), type = "logical", default = TRUE, action = "store_false", 
+    help = "Do not compress final output" ),
+  optparse::make_option(c("-c", "--css_files"), type = "character", default=NULL,
+    help = "Comma-separated paths to css files to include"),
   optparse::make_option(c("-j", "--js_files"), type = "character", default=NULL,
-    help="Path to javascript files that must be included. Use ',' as path separator for each file"),
- optparse::make_option(c("-C", "--css_cdn"), type = "character", default=NULL,
-    help="URL to css CDNs that must be included. Use ',' as path separator for each file"),
- optparse::make_option(c("-J", "--js_cnd"), type = "character", default=NULL,
-    help="URL to javascript CDNs that must be included. Use ',' as path separator for each file"),
- optparse::make_option(c("-m", "--menu"), type = "character", default="contents_list",
-    help="Indicate if indexed content must be a contents list (contents_list) or a menu (menu)")
+    help = "Comma-separated paths to js file to inclide"),
+  optparse::make_option(c("-C", "--css_cdn"), type = "character", default=NULL,
+    help = "Comma-separated URLs to css CDNs to include"),
+  optparse::make_option(c("-J", "--js_cnd"), type = "character", default=NULL,
+    help = "Comma-separated URLs to javascript CDNs to include"),
+  optparse::make_option(c("-m", "--menu"), type = "character", default="contents_list",
+    help = "Type of index content. Values: \"contents_list\" (the default) or \"menu\"")
   )
 
 opt <- optparse::parse_args(optparse::OptionParser(option_list=option_list))
